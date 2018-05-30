99b9b63e732769170d2eda8bbb8a74356b58e088
Lei Mao <dukeleimao@gmail.com>
2018-05-30 17:08:51 -0500

    add tensorboard argument

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-
-
 import tensorflow as tf
 import cv2
 import os
@@ -10,7 +8,7 @@ import time
 from utils import load_data, sample_train_data, image_scaling, image_scaling_inverse
 from model import CycleGAN
 
-def train(img_A_dir, img_B_dir, model_dir, model_name, random_seed, validation_A_dir, validation_B_dir, output_dir):
+def train(img_A_dir, img_B_dir, model_dir, model_name, random_seed, validation_A_dir, validation_B_dir, output_dir, tensorboard_log_dir):
 
     np.random.seed(random_seed)
 
@@ -30,7 +28,7 @@ def train(img_A_dir, img_B_dir, model_dir, model_name, random_seed, validation_A
         if not os.path.exists(validation_B_output_dir):
             os.makedirs(validation_B_output_dir)
 
-    model = CycleGAN(input_size = input_size, num_filters = num_filters, mode = 'train')
+    model = CycleGAN(input_size = input_size, num_filters = num_filters, mode = 'train', log_dir = tensorboard_log_dir)
 
     dataset_A_raw = load_data(img_dir = img_A_dir, load_size = 256)
     dataset_B_raw = load_data(img_dir = img_B_dir, load_size = 256)
@@ -96,6 +94,7 @@ if __name__ == '__main__':
     validation_A_dir_default = './data/horse2zebra/testA'
     validation_B_dir_default = './data/horse2zebra/testB'
     output_dir_default = './validation_output'
+    tensorboard_log_dir_default = './log'
 
     parser.add_argument('--img_A_dir', type = str, help = 'Directory for A images.', default = img_A_dir_default)
     parser.add_argument('--img_B_dir', type = str, help = 'Directory for B images.', default = img_B_dir_default)
@@ -105,6 +104,7 @@ if __name__ == '__main__':
     parser.add_argument('--validation_A_dir', type = str, help = 'Convert validation A images after each training epoch. If set none, no conversion would be done during the training.', default = validation_A_dir_default)
     parser.add_argument('--validation_B_dir', type = str, help = 'Convert validation B images after each training epoch. If set none, no conversion would be done during the training.', default = validation_B_dir_default)
     parser.add_argument('--output_dir', type = str, help = 'Output directory for converted validation images.', default = output_dir_default)
+    parser.add_argument('--tensorboard_log_dir', type = str, help = 'TensorBoard log directory.', default = tensorboard_log_dir_default)
 
     argv = parser.parse_args()
 
@@ -116,5 +116,6 @@ if __name__ == '__main__':
     validation_A_dir = None if argv.validation_A_dir == 'None' or argv.validation_A_dir == 'none' else argv.validation_A_dir
     validation_B_dir = None if argv.validation_B_dir == 'None' or argv.validation_B_dir == 'none' else argv.validation_B_dir
     output_dir = argv.output_dir
+    tensorboard_log_dir = argv.tensorboard_log_dir
 
-    train(img_A_dir = img_A_dir, img_B_dir = img_B_dir, model_dir = model_dir, model_name = model_name, random_seed = random_seed, validation_A_dir = validation_A_dir, validation_B_dir = validation_B_dir, output_dir = output_dir)
+    train(img_A_dir = img_A_dir, img_B_dir = img_B_dir, model_dir = model_dir, model_name = model_name, random_seed = random_seed, validation_A_dir = validation_A_dir, validation_B_dir = validation_B_dir, output_dir = output_dir, tensorboard_log_dir = tensorboard_log_dir)
